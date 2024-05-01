79f7e1e0703d8bba8c6256f973428e3b07ba1790
Josequake <jose_quake@hotmail.com>
2024-05-01 15:20:37 -0600

    4 entrega

--- js/script.js
+++ js/script.js
@@ -2,12 +2,23 @@ let nombrecompleto = document.getElementById("NOM");
 let telefono = document.getElementById("TE");
 let correo = document.getElementById("CO");
 let lugarderesidencia = document.getElementById("LR");
-let habilidadespersonales = document.getElementById("HP");
-let experiencialaboral = document.getElementById("EL");
-let educacion = document.getElementById("ED");
-let pasatiempos = document.getElementById("PA");
+let experiencialaboral1 = document.getElementById("EL1");
+let experiencialaboral2 = document.getElementById("EL2");
+let experiencialaboral3 = document.getElementById("EL3");
+let habilidadpersonal1 = document.getElementById("HP1");
+let habilidadpersonal2 = document.getElementById("HP2");
+let habilidadpersonal3 = document.getElementById("HP3");
+let educacion1 = document.getElementById("ED1");
+let educacion2 = document.getElementById("ED2");
+let pasatiempo1 = document.getElementById("PA1");
+let pasatiempo2 = document.getElementById("PA2");
 let boton = document.getElementById("boton");
 let text = document.getElementById("texto");
+let porcentaje1 = document.getElementById("por1");
+let porcentaje2 = document.getElementById("por2");
+let porcentaje3 = document.getElementById("por3");
+let porcentaje4 = document.getElementById("por4");
+let porcentaje5 = document.getElementById("por5");
 
 
 
@@ -18,9 +29,20 @@ boton.addEventListener("click", function(){
     localStorage.setItem('Telefono', telefono.value )
     localStorage.setItem('Correo', correo.value )
     localStorage.setItem('LugarDeResidencia', lugarderesidencia.value )
-    localStorage.setItem('HabilidadesPersonales', habilidadespersonales.value )
-    localStorage.setItem('ExperienciaLaboral', experiencialaboral.value )
-    localStorage.setItem('Educacion', educacion.value )
-    localStorage.setItem('Pasatiempos', pasatiempos.value )
+    localStorage.setItem('ExperienciaLaboral1', experiencialaboral1.value )
+    localStorage.setItem('ExperienciaLaboral2', experiencialaboral2.value )
+    localStorage.setItem('ExperienciaLaboral3', experiencialaboral3.value )
+    localStorage.setItem('HabilidadPersonal1', habilidadpersonal1.value )
+    localStorage.setItem('HabilidadPersonal2', habilidadpersonal2.value )
+    localStorage.setItem('HabilidadPersonal3', habilidadpersonal3.value )
+    localStorage.setItem('Educacion1', educacion1.value )
+    localStorage.setItem('Educacion2', educacion2.value )
+    localStorage.setItem('Pasatiempo1', pasatiempo1.value )
+    localStorage.setItem('Pasatiempo2', pasatiempo2.value )
+    localStorage.setItem('Porcentaje1', porcentaje1.value)
+    localStorage.setItem('Porcentaje2', porcentaje2.value)
+    localStorage.setItem('Porcentaje3', porcentaje3.value)
+    localStorage.setItem('Porcentaje4', porcentaje4.value)
+    localStorage.setItem('Porcentaje5', porcentaje5.value)
     
 })
--- js/script2.js
+++ js/script2.js
@@ -2,10 +2,21 @@ let nombrecompleto = document.getElementById("nombrecompleto")
 let telefono = document.getElementById("telefono")
 let correo = document.getElementById("correo")
 let lugarderesidencia = document.getElementById("lugarderesidencia")
-let habilidadespersonales= document.getElementById("habilidadespersonales")
-let experiencialaboral = document.getElementById("experiencialaboral")
-let educacion = document.getElementById("educacion")
-let pasatiempos = document.getElementById("pasatiempos")
+let experiencialaboral1 = document.getElementById("experiencialaboral1")
+let experiencialaboral2 = document.getElementById("experiencialaboral2")
+let experiencialaboral3 = document.getElementById("experiencialaboral3")
+let habilidadpersonal1= document.getElementById("habilidadpersonal1")
+let habilidadpersonal2= document.getElementById("habilidadpersonal2")
+let habilidadpersonal3= document.getElementById("habilidadpersonal3")
+let educacion1 = document.getElementById("educacion1")
+let educacion2 = document.getElementById("educacion2")
+let pasatiempo1 = document.getElementById("pasatiempo1")
+let pasatiempo2 = document.getElementById("pasatiempo2")
+let porcentaje1 = document.getElementById("porcentaje1")
+let porcentaje2 = document.getElementById("porcentaje2")
+let porcentaje3 = document.getElementById("porcentaje3")
+let porcentaje4 = document.getElementById("porcentaje4")
+let porcentaje5 = document.getElementById("porcentaje5")
 
 function llamar() {
 
@@ -20,18 +31,51 @@ function llamar() {
 
     let lugarderesidenciaL = localStorage.getItem("LugarDeResidencia")
     lugarderesidencia.innerHTML = lugarderesidenciaL;
+    
+    let experiencialaboral1L = localStorage.getItem("ExperienciaLaboral1")
+    experiencialaboral1.innerHTML = experiencialaboral1L;
+    
+    let experiencialaboral2L = localStorage.getItem("ExperienciaLaboral2")
+    experiencialaboral2.innerHTML = experiencialaboral2L;
+   
+    let experiencialaboral3L = localStorage.getItem("ExperienciaLaboral3")
+    experiencialaboral3.innerHTML = experiencialaboral3L;
 
-    let habilidadespersonalesL = localStorage.getItem("HabilidadesPersonales")
-    habilidadespersonales.innerHTML = habilidadespersonalesL;
+    let habilidadpersonal1L = localStorage.getItem("HabilidadPersonal1")
+    habilidadpersonal1.innerHTML = habilidadpersonal1L;
+    
+    let habilidadpersonal2L = localStorage.getItem("HabilidadPersonal2")
+    habilidadpersonal2.innerHTML = habilidadpersonal2L;
+    
+    let habilidadpersonal3L = localStorage.getItem("HabilidadPersonal3")
+    habilidadpersonal3.innerHTML = habilidadpersonal3L;
 
-    let experiencialaboralL = localStorage.getItem("ExperienciaLaboral")
-    experiencialaboral.innerHTML = experiencialaboralL;
+    let educacion1L = localStorage.getItem("Educacion1")
+    educacion1.innerHTML = educacion1L;
 
-    let educacionL = localStorage.getItem("Educacion")
-    educacion.innerHTML = educacionL;
+    let educacion2L = localStorage.getItem("Educacion2")
+    educacion2.innerHTML = educacion2L;
 
-    let pasatiemposL = localStorage.getItem("Pasatiempos")
-    pasatiempos.innerHTML = pasatiemposL;
+    let pasatiempo1L = localStorage.getItem("Pasatiempo1")
+    pasatiempo1.innerHTML = pasatiempo1L;
+
+    let pasatiempo2L = localStorage.getItem("Pasatiempo2")
+    pasatiempo2.innerHTML = pasatiempo2L;
+
+    let porcentaje1L = localStorage.getItem("Porcentaje1")
+    porcentaje1.innerHTML = porcentaje1L;
+    
+    let porcentaje2L = localStorage.getItem("Porcentaje2")
+    porcentaje2.innerHTML = porcentaje2L;
+   
+    let porcentaje3L = localStorage.getItem("Porcentaje3")
+    porcentaje3.innerHTML = porcentaje3L;
+   
+    let porcentaje4L = localStorage.getItem("Porcentaje4")
+    porcentaje4.innerHTML = porcentaje4L;
+   
+    let porcentaje5L = localStorage.getItem("Porcentaje5")
+    porcentaje5.innerHTML = porcentaje5L;
 }
 
 
